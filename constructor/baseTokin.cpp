1779df44aec5a09310b803f764987b2d0f46d389
MarkProMaster229 <164912605+MarkProMaster229@users.noreply.github.com>
2025-08-25 14:34:46 +0300

    new

---
+++ constructor/baseTokin.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#pragma once
+#include <vector>
+#include <string>
+#include <cstdint>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    return 0;
+}
+
+class tokinizer
+{
+    //словарь как ASCII и в навесок резервируем места старше 255(конец нумерации последнего символа)
+     struct Vocab
+     {
+        /*
+        Вокаб даёт нам правила соответствия:
+        если встретили спец-токен → добавляем его id (например, EOS = 258).
+        если встретили символ → берём его байтовое значение (например, 'h' = 104).
+        
+        Таким образом токенизатор не придумывает id случайно, он просто смотрит в таблицу «символ → число».
+        */
+        static constexpr int PAD = 256;//padding (добивание пустыми местами в батче, когда длины разные).
+        static constexpr int BOS = 257;//begin of sequence, токен начала последовательности.
+        static constexpr int EOS = 258;//end of sequence, токен конца последовательности.
+        static constexpr int UNK = 259;//unknown (неизвестный токен, если что-то не получилось закодировать).
+        
+        static constexpr int BASE = 256;//просто размер базовой части (все байты).
+        static constexpr int SIZE = 260;//общее количество токенов = 256 байтов + 4 спец-токена.
+     };
+     // основа токенизатора 
+     struct ByteTokinizer
+     {
+        vector<int> encode(const string& text, bool add_bos=true, bool add_eos=true){
+            //Создаём пустой список токенов (ids)
+            //Если нужно — сразу добавляем спец-токен начала (BOS = 257).
+            vector<int> ids;
+            if(add_bos) ids.push_back(Vocab::BOS);
+            
+            /*
+            Берём строку посимвольно.
+            Каждый символ в C++ хранится в виде байта (число 0–255).
+            Кладём это число в список токенов.
+            Например "hi" - 'h' = 104, 'i' = 105.
+            */
+            for(unsigned char c: text)
+            {
+                ids.push_back((int)c);// каждый байт напрямую в id
+            }
+            //брат если слово кончилось кинь токен конца последовательности епта
+            //Возвращаем готовый список.
+            //Итого "hi" → [257, 104, 105, 258].
+            //где 257 - сообщает что это начало, 258 - конец, 104 -"h", 105 - "i" 
+            if (add_eos) ids.push_back(Vocab::EOS);
+            
+            return ids;
+            
+        }
+
+
+     };
+     
+
+};
