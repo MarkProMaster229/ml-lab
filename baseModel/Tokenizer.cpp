68d8e4f5622181eafd7704353702a1c1f5e69b89
MarkProMaster229 <romtomgmai@gmail.com>
2025-09-11 16:29:36 +0300

    fix

--- baseModel/Tokenizer.cpp
+++ baseModel/Tokenizer.cpp
@@ -12,30 +12,36 @@ class Tokenizer
         static constexpr int BOS = 257;//begin of sequence, токен начала последовательности.
         static constexpr int EOS = 258;//end of sequence, токен конца последовательности.
         static constexpr int UNK = 259;//unknown (неизвестный токен, если что-то не получилось закодировать).
-        
+
         static constexpr int BASE = 256;//просто размер базовой части (все байты).
         static constexpr int SIZE = 260;//общее количество токенов = 256 байтов + 4 спец-токена.
         void myTokinezer(string text)
         {
-            unordered_map<char, int> mapAllIcon;
+            unordered_map<char, int> mapAllIconChar;
+            unordered_map<string, int> mapAllIconStr;
             vector<int> finalyWorld;
             int rez = 7;
-            mapAllIcon['[PAD]'] = PAD;
-            mapAllIcon['[BOS]'] = BOS;
-            mapAllIcon['[EOS]'] = EOS;
-            mapAllIcon['[UNK]'] = UNK;
-            mapAllIcon['[BASE]'] = BASE;
-            mapAllIcon['[SIZE]'] = SIZE;
-            
-            
+
+            // Спец-токены (строки)
+            mapAllIconStr["[PAD]"] = PAD;
+            mapAllIconStr["[BOS]"] = BOS;
+            mapAllIconStr["[EOS]"] = EOS;
+            mapAllIconStr["[UNK]"] = UNK;
+            mapAllIconStr["[BASE]"] = BASE;
+            mapAllIconStr["[SIZE]"] = SIZE;
+
+            // Все символы
+            for (int i = 0; i < 256; ++i)
+                mapAllIconChar[(char)i] = i;
+
             vector<int> token;
             token.push_back(BOS);
             for (char i:text)
             {
-                if(mapAllIcon.count(i))
-                   token.push_back(mapAllIcon[i]);
+                if(mapAllIconChar.count(i))
+                   token.push_back(mapAllIconChar[i]);
                 else
-                token.push_back(UNK);
+                   token.push_back(UNK);
             }
             token.push_back(EOS);
             for (int i = 0; i<token.size(); i++)
@@ -43,8 +49,6 @@ class Tokenizer
                 cout<<token[i] <<" ";
             }
             cout << endl;
-            
-
         }
-    
+
 };
