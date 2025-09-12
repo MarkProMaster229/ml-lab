c35e7182ef65b2e01cf3948b4003b26e6f7e86cc
MarkProMaster229 <romtomgmai@gmail.com>
2025-09-12 14:24:53 +0300

    newTensor

--- baseModel/Tensor.cpp
+++ baseModel/Tensor.cpp
@@ -3,12 +3,35 @@
 #include <string>
 #include <cstdint>
 #include <unordered_map>
+#include <assert.h>
 using namespace std;
 
 class Tensor
 {
     public:
+    vector<float> data;// хранит все числа
+    vector<int> shape;//размеры по осям, [batch, seq_len, embedding_dim]
+    vector<int> strides;// смещения по каждой оси
 
+     Tensor(int batch, int seq_len, int embedding_dim) {
+        shape = {batch, seq_len, embedding_dim};
+        // выделяем память под все элементы
+        data.resize(batch * seq_len * embedding_dim);
 
+        // считаем strides
+        strides.resize(3);
+        strides[2] = 1;  // последняя ось
+        strides[1] = embedding_dim;
+        strides[0] = seq_len * embedding_dim;
+    }
+
+        // доступ к элементу [i,j,k]
+    float& at(int i, int j, int k) {
+        assert(i >= 0 && i < shape[0]);
+        assert(j >= 0 && j < shape[1]);
+        assert(k >= 0 && k < shape[2]);
+        int index = i*strides[0] + j*strides[1] + k*strides[2];
+        return data[index];
+    }
 
 };
