08f77586930349413229655b01354ba4ffdedd98
MarkProMaster229 <romtomgmai@gmail.com>
2025-09-14 14:37:16 +0300

    fix_TEST

--- baseModel/initialization.cpp
+++ baseModel/initialization.cpp
@@ -8,23 +8,35 @@
 class initialization
 {
 public:
-    // метод генерации случайного тензора
-    Tensor freeRandom(const std::vector<int>& tokens, int embedding_dim)
+    // метод генерации случайного тензора для всех предложений
+    Tensor freeRandom(const std::vector<std::vector<int>>& all_tokens, int embedding_dim)
     {
         srand(time(nullptr));
 
-        int batch = 4;
-        int seq_len = tokens.size();
+        int batch = all_tokens.size();
 
-        Tensor ten(batch, seq_len, embedding_dim);
+        // Находим максимальную длину предложения
+        int max_seq_len = 0;
+        for (auto &tokens : all_tokens)
+            if (tokens.size() > max_seq_len)
+                max_seq_len = tokens.size();
 
-    for (int i = 0; i < batch; i++) {          // цикл по предложениям
-        for (int j = 0; j < seq_len; j++) {    // цикл по токенам
-            for (int k = 0; k < embedding_dim; k++) {
-                ten.at(i, j, k) = (float)rand() / RAND_MAX;
+        // Создаем тензор [batch, max_seq_len, embedding_dim]
+        Tensor ten(batch, max_seq_len, embedding_dim);
+
+        // Заполняем эмбеддинги случайными числами, добавляем паддинг 0 для коротких предложений
+        for (int i = 0; i < batch; i++) {
+            int seq_len = all_tokens[i].size();
+            for (int j = 0; j < max_seq_len; j++) {
+                for (int k = 0; k < embedding_dim; k++) {
+                    if (j < seq_len)
+                        ten.at(i, j, k) = (float)rand() / RAND_MAX;
+                    else
+                        ten.at(i, j, k) = 0.0f; // PAD
+                }
             }
         }
-    }
+
         return ten;
     }
 
@@ -33,14 +45,23 @@ public:
         Embeding em;
         std::vector<std::vector<int>> all_tokens = em.GetAnaliz();
 
-        int embedding_dim = 10;
-        Tensor t = freeRandom(all_tokens[0], embedding_dim);
+        if (all_tokens.empty()) {
+            std::cerr << "Нет предложений для обработки!" << std::endl;
+            return;
+        }
 
+        int embedding_dim = 10;
+
+        // Генерируем тензор для всех предложений
+        Tensor t = freeRandom(all_tokens, embedding_dim);
+
+        // Сохраняем и загружаем для проверки
         t.save("tensor.pt");
 
         Tensor t2;
         t2.load("tensor.pt");
 
         std::cout << "t2[0][0][0] = " << t2.at(0, 0, 0) << std::endl;
+        std::cout << "Shape: [ " << t2.shape[0] << " " << t2.shape[1] << " " << t2.shape[2] << " ]" << std::endl;
     }
 };
