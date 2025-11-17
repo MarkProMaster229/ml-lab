b8ee5da214704d2674bf0f1268d79ccdd9078b26
MarkProMaster229 <romtomgmai@gmail.com>
2025-11-17 13:12:06 +0300

    testFix

--- CNN/test_letters.py
+++ CNN/test_letters.py
@@ -7,71 +7,33 @@ from PIL import Image
 from safetensors.torch import load_file
 import string
 
-# -----------------------------
-# ТВОЯ МОДЕЛЬ 1в1
-# -----------------------------
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
         self.conv1 = nn.Conv2d(1,64,kernel_size=3,padding=1)
-        
-        #Делает уменьшение картинки вдвое
-        #для того чтоб второй слой ловил больше паттернов
-        #forward x = F.max_pool2d(x, 2)
-        
-        # Второй сверточный слой: 16 → 32 фильтра
-        #Берёт 16 входных «карт признаков» с предыдущего слоя → создаёт 32 новые карты признаков
-        #сеть - может комбинировать простые паттерны в сложные формы
         self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
-        
         self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
-        
         self.conv4 = nn.Conv2d(256, 512, kernel_size=3, padding=1)
-        
         self.conv5 = nn.Conv2d(512, 1024, kernel_size=3, padding=1)
-        #БАМ! не ожидали?
-        #self.pool = nn.AdaptiveAvgPool2d((8,8))
-        
-        #Flatten + Linear — полносвязный слой
-        #view превращает 3D-тензор (32×7×7) в 1D вектор
-        #forward x = x.view(x.size(0), -1)
-        #Linear — обычный нейронный слой: каждый вход соединяется со всеми 64 нейронами.
         self.fc1 = nn.Linear(1024*8*8, 512)
         self.fc2 = nn.Linear(512, 256)
         self.fc3 = nn.Linear(256, 128)
-        #выходной слой 
         self.fc4 = nn.Linear(128, 26)
         
 
     def forward(self, x):
-        #делает 16 карт признаков:
         x = F.relu(self.conv1(x))
-        #Делит картинку на 2
         x = F.max_pool2d(x,2)
-        #сеть получает уменьшенную картинку
         x = F.relu(self.conv2(x))
-        #снова размер делится на 2
         x = F.max_pool2d(x, 2)
-        
         x = F.relu(self.conv3(x))
         x = F.max_pool2d(x, 2)
-        
         x = F.relu(self.conv4(x))
-        
         x = F.relu(self.conv5(x))
-        #воооооот!
-        #x = self.pool(x)
-        
-        #print(x.shape)
-        
-        #Выравнивание
         x = x.view(x.size(0), -1)
-        #далее подать в полносвязный слой
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = F.relu(self.fc3(x))
-            
-        #выходной слой 
         x = self.fc4(x)
         return x
 
