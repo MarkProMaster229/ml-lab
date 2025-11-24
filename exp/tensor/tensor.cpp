6332897393a405873dc11abddc3feb563a18a335
MarkProMaster229 <romtomgmai@gmail.com>
2025-11-24 17:50:07 +0300

    name

--- exp/tensor/tensor.cpp
+++ exp/tensor/tensor.cpp
@@ -7,21 +7,27 @@
 
 class Tensor 
 {
+    size_t embedding_dim;
     std::vector<float>vec;
     std::size_t count;
 public:
 
-    Tensor(std::vector<float>vec, std::size_t count)
+    Tensor(std::vector<float>vec, std::size_t count,size_t embedding_dim)
     {
         this->vec = vec; 
         this->count = count;
-    
+        this->embedding_dim = embedding_dim;
     }
 
-    //std::vector embeddingLookup()
-    //{
-    
-    //}
+    std::vector<float> embeddingLookup(size_t token)
+    {
+        std::vector<float> result(embedding_dim);
+        for (size_t i = 0; i < embedding_dim; i++)
+        {
+            //вот так будет выглядеть моя структура внутри тензора 
+            result[i] = vec[token * embedding_dim + i];
+        }
+    }
     
 
     class Autograd
